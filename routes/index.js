65b5e534a410161a7c480a076cbc8c87b901e4ae
lemieux <Berthier.lemieux@eatech.fi>
2019-01-19 15:26:47 +0200

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+var sum = require('../business_logic/sum.js');
+
 var express = require('express');
 var router = express.Router();
 
@@ -10,6 +12,12 @@ router.get('/hello', function(req, res, next) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end(`Hello ${req.query.name}`);
+});
+
+router.get('/sum', function(req, res, next) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(sum(1,2).toString);
 });
 
 module.exports = router;
